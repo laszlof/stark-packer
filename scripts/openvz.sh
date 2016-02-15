414ef9b49b1794ecdb516e47a9a3853bcf04ec1d
Frank Laszlo <flaszlo@nexcess.net>
2016-02-15 16:01:35 -0500

    kernel headers

--- scripts/openvz.sh
+++ scripts/openvz.sh
@@ -2,6 +2,6 @@ yum install -y wget
 wget -P /etc/yum.repos.d/ http://ftp.openvz.org/openvz.repo
 rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-yum -y install gcc make gcc-c++ perl bzip2 vzkernel
+yum -y install gcc make gcc-c++ perl bzip2 vzkernel vzkernel-headers
 reboot
 sleep 60
