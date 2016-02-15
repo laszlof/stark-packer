62ed2cf8d3919065f994919f1382f918ca27d38a
Frank Laszlo <flaszlo@nexcess.net>
2016-02-15 14:36:19 -0500

    remove unused base kernel update

--- scripts/openvz.sh
+++ scripts/openvz.sh
@@ -1,5 +1,7 @@
 wget -P /etc/yum.repos.d/ http://ftp.openvz.org/openvz.repo
 rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
-yum install vzkernel
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+yum -y install gcc make gcc-c++ perl wget bzip2 vzkernel
+yum -y update
 reboot
 sleep 60
