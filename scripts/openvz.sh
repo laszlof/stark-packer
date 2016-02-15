81403b37b9e645d7188f0eb12d9c6823196b71ef
Frank Laszlo <flaszlo@nexcess.net>
2016-02-15 15:22:23 -0500

    remove yum update, not needed

--- scripts/openvz.sh
+++ scripts/openvz.sh
@@ -2,6 +2,5 @@ wget -P /etc/yum.repos.d/ http://ftp.openvz.org/openvz.repo
 rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 yum -y install gcc make gcc-c++ perl wget bzip2 vzkernel
-yum -y update
 reboot
 sleep 60
