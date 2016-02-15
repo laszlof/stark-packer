5fcb5bdb0edfbe4d92945a1777e7326059dbf81b
Frank Laszlo <flaszlo@nexcess.net>
2016-02-15 15:39:42 -0500

    fuckin wget

--- scripts/openvz.sh
+++ scripts/openvz.sh
@@ -1,6 +1,7 @@
+yum install -y wget
 wget -P /etc/yum.repos.d/ http://ftp.openvz.org/openvz.repo
 rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
-yum -y install gcc make gcc-c++ perl wget bzip2 vzkernel
+yum -y install gcc make gcc-c++ perl bzip2 vzkernel
 reboot
 sleep 60
